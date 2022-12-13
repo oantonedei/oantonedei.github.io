87daea503015395fa382dfab434c9e58ac907dca
Antonedei Wonimidei Otoro <otoroj@engineer.com>
2022-12-13 06:45:44 -0600

    added the files for the classes and inheritance lab

---
+++ js/classes/savingsaccount.js
@@ -0,0 +1,29 @@
+class SavingsAccount extends Account {
+  constructor(number, interest) {
+    super(number);
+    this.interest = interest;
+  }
+  setInterest(intrest) {
+    this.intrest = interest;
+  }
+  getInterest() {
+    return this._interest;
+  }
+  addInterest() {
+    let intrestAmount = (this.getBalance() * this.interest) / 100;
+    this.deposit(intrestAmount);
+    return intrestAmount;
+  }
+
+  /**
+   * @returns {string} representation of this account
+   */
+  toString() {
+    return "Savings" + super.toString() + " interest rate: " + this.interest;
+  }
+
+  endOfMonth() {
+    let intrestAdded = this.addInterest();
+    return this.toString() + " interest: " + intrestAdded;
+  }
+}
